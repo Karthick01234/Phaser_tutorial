54638238ab4d00090f19fd7196d52afbe251da25
Karthick01234 <gkskarthi32@gmail.com>
2022-11-17 16:48:50 +0530

    parrot

--- js/PreloadAssests.js
+++ js/PreloadAssests.js
@@ -3,7 +3,7 @@
 		super('PreloadAssests');   
 	}
 	preload() {
-		this.load.image('sky', 'img/sky.png');
+		this.load.image('sky', 'img/img/c.jpg');
 		this.load.spritesheet('bird', 'img/parrot.png', { frameWidth: 64, frameHeight: 64 });
 		this.load.image('pipe', 'img/pipe.png');
 		this.load.image('pause', 'img/pause.png');
